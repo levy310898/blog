d47f3c31f3d30e12cd32c2ee18e6399835b639c9
levy310898 <levy310898@gmail.com>
2020-12-13 13:00:31 +0700

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 
 
+
 const app = express();
 const port = 3000;
+
+// config statis file
+app.use(express.static(path.join(__dirname, 'public')));
 
 // http logger
 app.use(morgan('combined'))
@@ -14,10 +18,9 @@ app.use(morgan('combined'))
 // Template engine
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
 
-app.set('views',path.join(__dirname,'resources/views'))
-
-console.log("path: " + path.join(__dirname, 'resources/views'));
+// get method
 app.get('/', (req, res) => {
   res.render('home');
 });
@@ -25,6 +28,8 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
   res.render('news');
 });
+
+// listen
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
